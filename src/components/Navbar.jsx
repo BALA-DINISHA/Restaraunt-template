fbf6b7d98363feefd0a3f9a876f820cb96b9b190
BALA-DINISHA <baladinisha@gmail.com>
2026-09-11 11:57:11 +0530

    Initial restaurant template

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar(){
+    return(
+        <nav>
+            <h2>AROMA</h2>
+            <div>
+                <a href="#home">Home</a>
+                <a href="">About</a>
+                <a href="">Menu</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
