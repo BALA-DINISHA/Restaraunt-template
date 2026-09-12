8e3960af6573780006c8128404bcd64800b5aa49
BALA-DINISHA <baladinisha@gmail.com>
2026-09-12 15:15:28 +0530

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-=======
+
 import { useState ,useEffect} from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import "./App.css";
 import OpeningHours from "./components/OpeningHours";
->>>>>>> c54b14516aa52fff67c52144747090feb498777b
+import About from "./components/About";
 function App() {
 
 
@@ -16,6 +13,7 @@ function App() {
       <Navbar restaurantName="AROMA" />
       <Hero />
      <OpeningHours />
+     <About/>
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+function About()
+{
+    return(
+        <section className="about">
+            
+
+                <div className="about-image">
+                    <img src="https://images.unsplash.com/photo-1515003197210-e0cd71810b5f" alt="Restaurant food" />
+                
+                </div>
+                <div className="about-content">
+                
+                <p>About US</p>
+                <h2>Good Food, Greate Moments</h2>
+                <p>
+                    At AROMA Restaurant, we belive that grate food brings pepple together. we serve delicious dishes prepared with fresh ingredients and care.
+                </p>
+
+                        <button>Discover More</button>
+                </div>
+                
+           
+        </section>
+    )
+}
+export default About
