37cdbbb37806ad54a583083bf41caab9a0fa28ee
BALA-DINISHA <baladinisha@gmail.com>
2026-09-15 15:42:37 +0530

    Update App component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,56 @@
+import { useState,useEffect } from "react";
 
 function Navbar(props){
+
+    const [menuOpen,setMenuOpen]=useState(false)
     const navItems = ["Home", "About", "Menu", "Gallery","Events","Contact"];
+    const [scrolled,setScrolled]=useState(false)
+
+    useEffect(()=>
+    {
+        const handleScroll=()=>
+        {
+            if(window.scrollY>0)
+            {
+                setScrolled(true)
+            }
+            else
+            {
+                setScrolled(false)
+            }
+        }
+        window.addEventListener("scroll",handleScroll)
+
+        return()=>
+        {
+            window.removeEventListener("scroll",handleScroll)
+        }
+
+    },[])
     return(
-        <nav className="navbar">
+        <nav className={`navbar ${scrolled ? "scrolled":""}`}>
             
-            <h2 className="logo">{props.restaurantName}</h2>
-            <div className="nav-links">
+           <div className="logo">
+            {props.restaurantName}
+            </div>
+
+            <button className="menu-button" onClick={
+                ()=>setMenuOpen(!menuOpen)
+            }>
+                 {menuOpen? "✕" : "☰" }
+            </button>
+
+
+            <div className={`nav-links ${menuOpen?"open":""}`}>
                 {
                     navItems.map(item=>(
-                        <a key={item} href={`#${item.toLowerCase()}`}>{item}</a>
+
+
+                        <a key={item} href={`#${item.toLowerCase()}`} onClick={()=>
+                        {
+                            setMenuOpen(false)
+                        }
+                        }>{item}</a>
                     ))
                 }
             </div>
